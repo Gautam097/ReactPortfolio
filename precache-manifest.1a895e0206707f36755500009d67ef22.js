8b479ac8124fc67d177107b248dc18a60d528c44
Gautam097 <gautamcse097@gmail.com>
2023-06-12 00:42:54 +0530

    Updates

---
+++ precache-manifest.1a895e0206707f36755500009d67ef22.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "243660b0a0affa023ffc",
+    "url": "/Gautam097/ReactPortfolio/static/css/main.627ee2f5.chunk.css"
+  },
+  {
+    "revision": "243660b0a0affa023ffc",
+    "url": "/Gautam097/ReactPortfolio/static/js/main.f7addf5f.chunk.js"
+  },
+  {
+    "revision": "a3fd9257ce41a19ba0a4",
+    "url": "/Gautam097/ReactPortfolio/static/js/runtime~main.028a0de7.js"
+  },
+  {
+    "revision": "fdad373902f02371fdff",
+    "url": "/Gautam097/ReactPortfolio/static/js/2.23882927.chunk.js"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "/Gautam097/ReactPortfolio/static/media/portfolio1.d1e8fb80.jpg"
+  },
+  {
+    "revision": "13db5e2202aa39b783fded70d877b662",
+    "url": "/Gautam097/ReactPortfolio/static/media/portfolio2.13db5e22.jpg"
+  },
+  {
+    "revision": "ce79413f3dfb4b9ab668d3236a4a04d1",
+    "url": "/Gautam097/ReactPortfolio/static/media/portfolio3.ce79413f.jpg"
+  },
+  {
+    "revision": "216d621e81ce024634aae65c2f64f993",
+    "url": "/Gautam097/ReactPortfolio/static/media/cv.216d621e.pdf"
+  },
+  {
+    "revision": "6c9986833f65c4a7d5ade53b8ca0b867",
+    "url": "/Gautam097/ReactPortfolio/static/media/gh2.6c998683.png"
+  },
+  {
+    "revision": "b4d6dba009e47365de3a7f9a66870074",
+    "url": "/Gautam097/ReactPortfolio/static/media/me-about2.b4d6dba0.jpg"
+  },
+  {
+    "revision": "04dbe3ab7df1162d6c760d973af4ad6a",
+    "url": "/Gautam097/ReactPortfolio/static/media/portfolio4.04dbe3ab.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "/Gautam097/ReactPortfolio/static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/Gautam097/ReactPortfolio/static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "96a3f2959f5f1cd24f8f30519c4ef039",
+    "url": "/Gautam097/ReactPortfolio/index.html"
+  }
+];
